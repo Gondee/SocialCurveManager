6f0ae0bd5669383fadd70842615a2e158cac942d
Josh <joshuackruger@gmail.com>
2015-10-09 02:31:56 +0000

    test edit

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Workspace
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # -- all .rb files in that directory are automatically loaded. Test
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
