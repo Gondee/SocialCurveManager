da9feabbfa0f228a060a8b636eee2c641ff84a02
Thomas Scott Jasper <scojasp@gmail.com>
2015-10-21 01:24:58 +0000

    Some initial routing for application.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :logged_in_user, only: []
+  def index
+    
+  end
+  def about
+    
+  end
+  def contact
+    
+  end
+  def login
+    
+  end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  # include SessionsHelper
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  root                   'application#index'
+  get 'index'         => 'application#index'
+  get 'about'         => 'application#about'
+  get 'contact'       => 'application#contact'
+  #get 'signup'       => 'users#new'
+  #get 'login'        => 'sessions#new'
+  #post 'login'       => 'sessions#create'
+  #delete 'logout'    => 'sessions#destroy'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +62,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  
 end
