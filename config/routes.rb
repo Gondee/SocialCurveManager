9980a329be4b3ea0df7bba22a595aeb0d75c3607
lef592 <lef592@tamu.edu>
2015-10-25 22:22:22 +0000

    Added Static Pages, Puma, Minor Fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
-  #get 'sessions/new'
+  root 'static_pages#home'
+  get 'help'    => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
+
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
 
   resources :generatedlinks
   resources :links
