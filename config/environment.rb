8e9ca588665f74751ca1186b8efd35dba9764e1b
Thomas Scott Jasper <scojasp@gmail.com>
2015-11-04 23:53:05 +0000

    Whoops

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-# For the mailer to work.
-config.action_mailer.default_url_options = { :host => 'scdp.heroku.com' }
