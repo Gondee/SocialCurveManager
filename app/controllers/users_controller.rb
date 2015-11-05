78ffb1b685ab029743a57f285c33d4ea5ee88f65
Thomas Scott Jasper <scojasp@gmail.com>
2015-11-05 00:38:13 +0000

    Segmentation of pages into renderings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :dashboard]
+  before_action :logged_in_user, only: [:edit, :update, :destroy, :dashboard]
   before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,  only: :destroy
+  before_action :admin_user,  only:    [:destroy, :index]
  # before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
