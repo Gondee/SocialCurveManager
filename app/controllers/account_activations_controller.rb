3beee3b3fd9ec2403bbfa69e1fb3fdd9867cf089
Thomas Scott Jasper <scojasp@gmail.com>
2015-11-05 00:01:19 +0000

    Fixing authentication arguments

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -2,7 +2,8 @@ class AccountActivationsController < ApplicationController
     
   def edit
     user = User.find_by(email: params[:email])
-    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+    if user && !user.activated? && user.authenticated?(:activation)
+      #user.authenticated?(:activation, params[:id])
       user.activate
       log_in user
       flash[:success] = "Account activated!"
