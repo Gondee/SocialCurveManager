ec550edb824a5460c715338cffbe888804a3b291
Josh <joshuackruger@gmail.com>
2015-11-07 23:14:40 +0000

    Active Record Fix

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,4 @@
 class Link < ActiveRecord::Base
-    belongs_to publisher, dependent: :destroy
-    has_many generatedlinks, dependent: :destroy
+    belongs_to :publisher, dependent: :destroy
+    has_many :generatedlinks, dependent: :destroy
 end
