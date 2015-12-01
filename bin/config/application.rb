5a85558695cb6eef9b98d6e03f4e75bb4ff072b3
Thomas Scott Jasper <scojasp@gmail.com>
2015-12-01 02:53:59 +0000

    Finished for tonight

--- bin/config/application.rb
+++ bin/config/application.rb
@@ -22,7 +22,5 @@ module Workspace
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    
-    config.web_console.whiny_requests = false
   end
 end
--- bin/config/environments/development.rb
+++ bin/config/environments/development.rb
@@ -40,4 +40,6 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   
   #config.action_mailer.preview_path = "#{Rails.root}/app/mailer_previews"
+  
+  config.web_console.whiny_requests = false
 end
