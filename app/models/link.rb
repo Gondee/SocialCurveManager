f881dfbf3271fd4ee7f2679e01bdf72bc4f17107
Thomas Scott Jasper <scojasp@gmail.com>
2015-12-01 00:59:53 +0000

    Add title and category input to add link section for Publishers

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,4 +3,6 @@ class Link < ActiveRecord::Base
     has_many :generatedlinks, dependent: :destroy
     validates :url,  presence: true
     validates :cpm,  presence: true
+    validates :category, inclusion: { in: %w(Health Fitness Hipster Science Funny Happiness),
+        message: " was not a valid category" }
 end
