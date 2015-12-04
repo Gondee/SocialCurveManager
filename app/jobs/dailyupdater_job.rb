7268752a2d07d725a5793cd9272d2a0a10176229
Josh <joshuackruger@gmail.com>
2015-12-04 21:34:47 +0000

    working background worker

--- app/jobs/dailyupdater_job.rb
+++ app/jobs/dailyupdater_job.rb
@@ -2,6 +2,7 @@ class DailyupdaterJob < ActiveJob::Base
   queue_as :default
 
   def perform
+      
      allgens = Generatedlink.where("paidout = ? AND dead = ?", false, false ) 
      allgens.each do |g|
         if(g.created_at > Time.now - (g.expire).days)
@@ -9,22 +10,34 @@ class DailyupdaterJob < ActiveJob::Base
                 # Should Expire the link
                 g.expire = true
                 
-                totalclicks = getTotalClicks(g.url)
-                g.paidclicks = getTotalClicks(totalclicks)
+                #calling the API
+                Google::UrlShortener::Base.api_key = "AIzaSyDAT2500mxvTeoAVStBL5LebEfgqpDU8vw"
+                url = Google::UrlShortener::Url.new(:short_url => g.url)
+                url.expand!
+                totalclicks = url.analytics.all.short_url_clicks
                 
+                #update values
+                g.paidclicks = totalclicks
                 stat = Statistic.new
                 stat.generatedlink_id = g.id
-                stat.getTotalClicks(totalclicks)
+                stat.clicks = totalclicks
                 
                 #save the changes
                 g.save
                 stat.save
             else
+                
+                Google::UrlShortener::Base.api_key = "AIzaSyDAT2500mxvTeoAVStBL5LebEfgqpDU8vw"
+                url = Google::UrlShortener::Url.new(:short_url => g.url)
+                url.expand! 
+                totalclicks = url.analytics.all.short_url_clicks
+                
+                #add value
                 stat = Statistic.new
-                totalclicks = getTotalClicks(g.url)
                 stat.generatedlink_id = g.id
-                stat.getTotalClicks(totalclicks)
+                stat.clicks = totalclicks
                 stat.save
+                puts "HELPPPPPPPPPPPPPP"
             end
           
           
