b5ef2eedbf56667e7f202462b86dc01b9f4249ba
Thomas Scott Jasper <scojasp@gmail.com>
2015-12-05 21:07:14 +0000

    Fixed publisher id and formatting of note

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -2,4 +2,8 @@ class Publisher < ActiveRecord::Base
     has_many :links, dependent: :destroy
     has_many :clearing_houses, dependent: :destroy
     belongs_to :user, dependent: :destroy
+    def email
+        @user = User.find(user_id)
+        @user.email
+    end
 end
